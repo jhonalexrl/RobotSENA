24161edfb85ef56d5a9911f557ad7a387ba2d114
AlejandroMontilla <56898236+AlejandroMontilla@users.noreply.github.com>
2019-10-22 19:46:16 -0500

    Update Led.h

--- Led.h
+++ Led.h
@@ -2,11 +2,13 @@ const int PLED = 13;
 boolean LEDStatus = LOW;
 
 //---------configuración de la tarea LED
-void SetupLED() 
-{ LEDStatus = LOW; 
-  pinMode(PLED,OUTPUT);}
+void SetupLED()
+{ LEDStatus = LOW;
+  pinMode(PLED, OUTPUT);
+}
 
 //---------Actualización de la tarea LED1
 void LedIntermitente()
-{digitalWrite(PLED,LEDStatus);
-LEDStatus=!LEDStatus;}
+{ digitalWrite(PLED, LEDStatus);
+  LEDStatus = !LEDStatus;
+}
