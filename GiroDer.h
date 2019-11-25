26097a519389ffb8da8ad802ce05880faa99e166
Alejobolanoslopez <55285527+Alejobolanoslopez@users.noreply.github.com>
2019-11-25 14:27:05 -0500

    Arreglos Formato

--- GiroDer.h
+++ GiroDer.h
@@ -7,41 +7,38 @@
 AF_DCMotor motor(3);
 
 void setupGiroDer() {
-  Serial.begin(115200);        
+  Serial.begin(115200);
   Serial.println("Motor test!");
 
   // turn on motor
   motor.setSpeed(200);
- 
+
   motor.run(RELEASE);
 }
 
 void GiroDer() {
   uint8_t i;
-  
+
   Serial.print("tick");
-  
+
   motor.run(FORWARD);
-  for (i=0; i<255; i++) {
-    motor.setSpeed(i);  
- }
- 
-  for (i=255; i!=0; i--) {
-    motor.setSpeed(i);  
- }
-  
+  for (i = 0; i < 255; i++) {
+    motor.setSpeed(i);
+  }
+
+  for (i = 255; i != 0; i--) {
+    motor.setSpeed(i);
+  }
+
   Serial.print("tock");
 
   motor.run(BACKWARD);
-  for (i=0; i<255; i++) {
-    motor.setSpeed(i);    
- }
- 
-  for (i=255; i!=0; i--) {
-    motor.setSpeed(i);  
- }
-  
-
+  for (i = 0; i < 255; i++) {
+    motor.setSpeed(i);
+  }
+  for (i = 255; i != 0; i--) {
+    motor.setSpeed(i);
+  }
   Serial.print("tech");
   motor.run(RELEASE);
 }
