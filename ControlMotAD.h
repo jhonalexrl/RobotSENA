71e8dc39d5f2674ff5019a67d89a81338b6a959b
guidoportillo <56898284+guidoportillo@users.noreply.github.com>
2019-11-25 17:46:32 -0500

    Update ControlMotAD.h

--- ControlMotAD.h
+++ ControlMotAD.h
@@ -1,5 +1,26 @@
-#include <AFMotor.h>
+//Nombre del Autor: IMEEI - Luis Alejandro Montilla - Guido Roberto Portillo
+//Fecha Modificación: 25/11/2019
+//Uso del programa: Controla los movimientos de los motores a travez de los parametros creados en 
+//VOID ATRAS(BYTE VEL) Y VOID ADELANTE(BYTE VEL) aqui se controla la 
+//velocidad de los motores 1 y 3 los cuales renombramos como D e I.
 
+//motorI_setSpeed:Es a la velocidad que queremos que el motor de la izquierda se mueva.
+//motorD_setSpeed:Es a la velocidad que queremos que el motor de la derecha se mueva.
+
+//motorI.run_RELEASE:Es la variable de LANZAMIENTO o desde un lado o punto inicial al 
+//que se lo quiere iniciar el motor para que llegue a otro lado o punto hacia el lado izquierdo.
+
+//motorD.run_RELEASE:Es la variable de LANZAMIENTO o desde un lado o punto inicial al 
+//que se lo quiere iniciar el motor para que llegue a otro lado o punto hacia el lado derecho.
+
+  
+//Funcionamiento del programa: aqui esta la programacion sobre los motores de la shield Adafruit 
+//la cual controla los movimientos de los motores hacia adelante, atras, derecha e izquierda.
+
+//Doy fe que este ejercicio es de mi autoría, en caso de encontrar plagio la nota de todo mi
+//trabajo debe ser de NO APROBADO además de las respectivas sanciones a que haya lugar
+
+#include <AFMotor.h>
 AF_DCMotor motorI(3);   //motor de la izquierda (3)
 AF_DCMotor motorD(1);   //motor de la derecha (1)
 
