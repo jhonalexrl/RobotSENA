ecc0ca1413b77aff036222af640524f8a65758ca
hoyberjose <56847069+hoyberjose@users.noreply.github.com>
2019-11-30 13:12:24 -0500

    definicion pines
    definicion pines de button y sensor

--- Pines.h
+++ Pines.h
@@ -17,18 +17,27 @@
 #ifdef SHIELD_ADAFRUIT
 //#include <AFMotor.h>
 #include "ControlMotAD.h"
-#define BUTTONPIN 2     // the number of the pushbutton pin
+#define BUTTONPIN A3     // Configura el pin de entrada del boton
+// Configurar el pin en el que se conecta el sensor
+#define ECHO   A5 //El terminal A5 Esta conectado a ECHO.
+#define TRIGER A4 //El terminal A4 Esta conectado a TRIGER.
 #endif
 //*************************************************
 #ifdef SHIELD_TB6612
 #include <SparkFun_TB6612.h>
 #include "ControlMot.h"
-#define BUTTONPIN 12     // the number of the pushbutton pin
+#define BUTTONPIN 12   // Configura el pin de entrada del boton
+// Configurar el pin en el que se conecta el sensor
+#define ECHO   A5 //El terminal A5 Esta conectado a ECHO.
+#define TRIGER A4 //El terminal A4 Esta conectado a TRIGER.
 #endif
 //*************************************************
 #ifdef SHIELD_FUNDUMOTO
 #include "FunduMot.h"
-#define BUTTONPIN 3     // the number of the pushbutton pin
+#define BUTTONPIN 3    // Configura el pin de entrada del boton
+// Configurar el pin en el que se conecta el sensor
+#define ECHO  7 //El terminal A5 Esta conectado a ECHO.
+#define TRIGER 8 //El terminal A4 Esta conectado a TRIGER.
 #endif
 //*************************************************
 
@@ -47,8 +56,3 @@
 #define PLED 2
 
 // Configura el pin de entrada del boton
-#define BUTTONPIN 3     // the number of the pushbutton pin
-
-// Configurar el pin en el que se conecta el sensor
-#define ECHO   A5 //El terminal A5 Esta conectado a ECHO.
-#define TRIGER A4 //El terminal A4 Esta conectado a TRIGER.
